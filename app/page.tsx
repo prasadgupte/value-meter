b712f56e8481627d415178d7d6a7eff548885c3a
“Prasad” <code@prasadgupte.com>
2025-01-13 16:19:31 +0100

    stable before layout alignment

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 export default function Home() {
   const [attendees, setAttendees] = useState(6);
   const [hourlyRate, setHourlyRate] = useState(45);
+  const [annualSalary, setAnnualSalary] = useState(80000);    
   const [productPrice, setProductPrice] = useState(30);
   const [cost, setCost] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
@@ -17,6 +18,10 @@ export default function Home() {
   const [showToast, setShowToast] = useState(false);
   const [triggerFlyout, setTriggerFlyout] = useState(false);
   const [isSettingsCollapsed, setIsSettingsCollapsed] = useState(true);
+  const [showAlert, setShowAlert] = useState(false);
+  const [lastProductsToBeSold, setLastProductsToBeSold] = useState(0);
+  const [productName, setProductName] = useState('Tonieboxes');
+  const [productImageUrl, setProductImageUrl] = useState('https://res.cloudinary.com/tonies/image/fetch/f_auto,q_60,c_fill,b_rgb:ffffff,w_1336,h_1002/https://278163f382d2bab4b036-4f5ec62496a160f3570d3b6e48fc4516.ssl.cf3.rackcdn.com/Toniebox_red_d-DrP5n54q.png');
 
   useEffect(() => {
     let timer: number | null = null;
@@ -64,8 +69,10 @@ export default function Home() {
     setIsPaused(false);
     setElapsedTime(0);
     setCost(0);
+    setLastProductsToBeSold(productsToBeSold);
     setProductsToBeSold(1);
-  };
+    setShowAlert(true);
+    };
 
   const pauseTimer = () => {
     setIsPaused(!isPaused);
@@ -104,16 +111,30 @@ export default function Home() {
     return symbols[currency] || currency;
   };
 
+  const handleAnnualSalaryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const annualSalary = Number(e.target.value);
+    setAnnualSalary(annualSalary);
+    const calculatedHourlyRate = Math.round(annualSalary / (12 * 22 * 8)*1.3);
+    setHourlyRate(calculatedHourlyRate);
+  };
+
+
   return (
     <>
       <div className="md:container md:mx-auto min-h-screen flex w-full flex-col p-4 bg-gray-100 items-center">
-        <header className="w-full mb-4">
-          <h1 className="bg-gradient-to-r from-primary to-error bg-clip-text text-transparent font-black text-4xl text-center">⏰ Value Meter</h1>
+        <header className="w-full mt-4">
+          <h1 className="bg-gradient-to-r from-primary to-error bg-clip-text text-transparent font-black text-4xl text-center">⏰ Time is money 💰</h1>
         </header>
-        <blockquote className="w-full text-center mb-4">“Time is money.” – Benjamin Franklin</blockquote>
+        {showAlert && (
+          <div className="alert alert-warning shadow-lg mt-4">
+            <div>
+              <span>🤑 We spent the <strong>margin from {lastProductsToBeSold} {productName}</strong> for the last meeting. You can do better! 💪</span>
+            </div>
+          </div>
+        )}
         {!isRunning ? (
-          <div className="card bg-white p-6 rounded-lg">
-            <div className="mb-4">
+          <div className="bg-white p-6 rounded-lg">
+            <div className="mt-4">
               <label htmlFor="attendees" className="block text-sm font-medium text-gray-700">Attendees:</label>
               <input type="number" id="attendees" className="input mt-1 block w-full" value={attendees} onChange={(e) => setAttendees(Number(e.target.value))} />
               <input type="range" id="attendees" min="3" max="30" className="range range-error mt-1 block w-full" step="1" value={attendees} onChange={(e) => setAttendees(Number(e.target.value))}/>
@@ -153,12 +174,24 @@ export default function Home() {
                   </select>
                 </div>
                 <div className="mb-4">
-                  <label htmlFor="hourlyRate" className="block text-sm font-medium text-gray-700">Hourly Rate ({getCurrencySymbol(currency)}):</label>
+                  <label htmlFor="annualSalary" className="block text-sm font-medium text-gray-700">Avg. Annual Salary ({getCurrencySymbol(currency)}):</label>
+                  <input type="number" id="annualSalary" className="input mt-1 block w-full" value={annualSalary} onChange={handleAnnualSalaryChange} />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="hourlyRate" className="block text-sm font-medium text-gray-700">Avg. Hourly Rate ({getCurrencySymbol(currency)}):</label>
                   <input type="number" id="hourlyRate" className="input mt-1 block w-full" value={hourlyRate} onChange={(e) => setHourlyRate(Number(e.target.value))} />
                 </div>
                 <div className="mb-4">
                   <label htmlFor="productPrice" className="block text-sm font-medium text-gray-700">Profit per Product ({getCurrencySymbol(currency)}):</label>
                   <input type="number" id="productPrice" className="input mt-1 block w-full" value={productPrice} onChange={(e) => setProductPrice(Number(e.target.value))} />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="productName" className="block text-sm font-medium text-gray-700">Product Name (plural):</label>
+                  <input type="text" id="productName" className="input mt-1 block w-full" value={productName} onChange={(e) => setProductName(e.target.value)} />
+                </div>
+                <div className="mb-4">
+                  <label htmlFor="productImageUrl" className="block text-sm font-medium text-gray-700">Product Image URL (png, jpg, gif):</label>
+                  <input type="text" id="productImageUrl" className="input mt-1 block w-full" value={productImageUrl} onChange={(e) => setProductImageUrl(e.target.value)} />
                 </div>
               </div>
             )}
@@ -185,7 +218,7 @@ export default function Home() {
               
               
                 <div className="stat bg-primary  text-primary-content strong inline-block">
-                  <div className="stat-title text-primary-content"><strong>Tonieboxes needed to be sold to recover cost</strong></div>
+                  <div className="stat-title text-primary-content"><strong>{productName} needed to be sold to recover cost</strong></div>
                   <div className="stat-value">{productsToBeSold}</div>
                 </div>
             </div>
@@ -193,8 +226,8 @@ export default function Home() {
               <figure>
                 <img
                   className={`flex items-center ${!triggerFlyout ? 'animate-pulse' : 'animate-flyout'}`}
-                  src="https://res.cloudinary.com/tonies/image/fetch/f_auto,q_60,c_fill,b_rgb:ffffff,w_1336,h_1002/https://278163f382d2bab4b036-4f5ec62496a160f3570d3b6e48fc4516.ssl.cf3.rackcdn.com/10000058-UK-d-2FIkIQJk.png"
-                  alt="Toniebox" />
+                  src={productImageUrl}
+                  alt={productName} />
               </figure>
               {/*<div className="card-body">
                 <h2 className="card-title">
@@ -202,7 +235,6 @@ export default function Home() {
                   <div className="badge badge-secondary">NEW</div>
                 </h2>                
               </div>*/}
-              <div className="divider">xx</div>
             </div>  
                                        
               <div className="bg-base-100 items-center">
